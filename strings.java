c0e7a9913075fa899c3928dd5b1a2b8542afbd76
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 16:22:56 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        String s="poornachandra";
+        for(int i=0;i<s.length();i++)
+        {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
