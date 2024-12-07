7ec0119381a1a81b72fea3f404549aacb0b942dd
poorna30646 <thotipoornachandra@gmail.com>
2024-12-07 12:21:48 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class free
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            s+=arr[i];
+        }
+        System.out.println(s);
+        
+    }
+}  
